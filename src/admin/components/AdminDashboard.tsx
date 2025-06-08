483f00b5c6fbcec25c09089cfbfdccf13ca8dc53
miguelgonzalezzdev <miguelgonzalezdev@gmail.com>
2025-06-08 17:52:13 +0200

    Menú y sección de administradores

--- src/admin/components/AdminDashboard.tsx
+++ src/admin/components/AdminDashboard.tsx
@@ -1,8 +1,60 @@
+import { useState } from "react";
+
+const sections = ["usuarios", "libros", "solicitudes"]
 
 export function AdminDashboard() {
+    const [activeTab, setActiveTab] = useState("usuarios")
+
+    const renderContent = () => {
+        switch (activeTab) {
+            case "usuarios":
+                return (
+                    <div>
+                        <h2 className="text-2xl font-semibold mb-4">Usuarios</h2>
+                        <p>Lista y gestión de usuarios registrados en la plataforma.</p>
+                    </div>
+                )
+            case "libros":
+                return (
+                    <div>
+                        <h2 className="text-2xl font-semibold mb-4">Libros</h2>
+                        <p>Explora, añade o edita libros disponibles en la biblioteca.</p>
+                    </div>
+                )
+            case "solicitudes":
+                return (
+                    <div>
+                        <h2 className="text-2xl font-semibold mb-4">Solicitudes</h2>
+                        <p>Revisa las solicitudes enviadas por los usuarios para aprobación.</p>
+                    </div>
+                )
+            default:
+                return
+        }
+    }
+
     return (
-        <div className="flex-grow flex flex-col items-center justify-center m-10 lg:m-20 gap-10 lg:gap-20">
-            <h1>ADMIN</h1>
+        <div className="flex-grow flex flex-col items-center justify-center m-10 lg:m-20 gap-10 lg:gap-20 text-neutral-900 dark:text-neutral-50">
+            <section className="w-full bg-white dark:bg-neutral-600 rounded-2xl p-8 md:p-12 shadow-md border border-gray-200 dark:border-neutral-700 flex flex-col gap-8">
+                <nav className="flex justify-center gap-6 border-b border-gray-300 dark:border-neutral-500 pb-4">
+                    {sections.map((tab) => (
+                        <button
+                            key={tab}
+                            onClick={() => setActiveTab(tab)}
+                            className={`px-4 py-2 font-bold rounded-lg transition ${activeTab === tab
+                                ? "bg-blue-600 text-white shadow-md"
+                                : " hover:text-blue-600"
+                                }`}
+                            aria-current={activeTab === tab ? "page" : undefined}
+                        >
+                            {tab.charAt(0).toUpperCase() + tab.slice(1)}
+                        </button>
+                    ))}
+                </nav>
+
+                <article className="text-center">{renderContent()}</article>
+            </section>
+
         </div>
     )
 }
--- src/core/components/Header.tsx
+++ src/core/components/Header.tsx
@@ -3,9 +3,11 @@ import { OpenMenuIcon } from "../icons/OpenMenuIcon.tsx"
 import { CloseMenuIcon } from "../icons/CloseMenuIcon.tsx"
 import { Link } from "react-router";
 import { Logo } from "./Logo.tsx";
+import { useAuthStore } from "../store/authStore.ts";
 
-export function Header () {
+export function Header() {
     const [isMenuOpen, setIsMenuOpen] = useState(false)
+    const isAdmin = useAuthStore(state => state.isAdmin)
 
     const handleToggleMenu = () => {
         setIsMenuOpen(!isMenuOpen)
@@ -18,13 +20,13 @@ export function Header () {
     return (
         <header className="bg-neutral-200 dark:bg-neutral-700 p-2 sticky top-0 z-900">
             <div className="container mx-auto flex justify-between items-center">
-                <Link  to="/" >
-                    <Logo className="max-h-8"/>
+                <Link to="/" >
+                    <Logo className="max-h-8" />
                 </Link >
-                
+
                 {/* Menú móvil */}
                 <button onClick={handleToggleMenu} className="lg:hidden px-2 focus:outline-none">
-                    { isMenuOpen 
+                    {isMenuOpen
                         ? <CloseMenuIcon />
                         : <OpenMenuIcon />
                     }
@@ -32,20 +34,30 @@ export function Header () {
 
                 {/* Menú de navegación visible solo en pantallas grandes */}
                 <nav className="hidden lg:flex space-x-4 text-neutral-900 dark:text-neutral-50">
-                    <Link  to="/" className="px-4 py-2 hover:bg-neutral-300 hover:dark:bg-neutral-600 rounded font-medium">Home</Link >
-                    <Link  to="/search" className="px-4 py-2 hover:bg-neutral-300 hover:dark:bg-neutral-600 rounded font-medium">Buscar</Link >
-                    <Link  to="/review" className="px-4 py-2 hover:bg-neutral-300 hover:dark:bg-neutral-600 rounded font-medium">Reseña</Link >
-                    <Link  to="/profile" className="px-4 py-2 hover:bg-neutral-300 hover:dark:bg-neutral-600 rounded font-medium">Perfil</Link >
+                    <Link to="/" className="px-4 py-2 hover:bg-neutral-300 hover:dark:bg-neutral-600 rounded font-medium">Home</Link >
+                    <Link to="/search" className="px-4 py-2 hover:bg-neutral-300 hover:dark:bg-neutral-600 rounded font-medium">Buscar</Link >
+                    {isAdmin
+                        ? <Link to="/admin" className="px-4 py-2 hover:bg-neutral-300 hover:dark:bg-neutral-600 rounded font-medium">Gestión</Link >
+                        : <>
+                            <Link to="/review" className="px-4 py-2 hover:bg-neutral-300 hover:dark:bg-neutral-600 rounded font-medium">Reseña</Link >
+                            <Link to="/profile" className="px-4 py-2 hover:bg-neutral-300 hover:dark:bg-neutral-600 rounded font-medium">Perfil</Link >
+                          </>
+                    }
                 </nav>
             </div>
 
             {/* Menú desplegable en móviles con transición */}
             <div className={`lg:hidden transition-all duration-200 ease-in-out ${isMenuOpen ? 'max-h-screen opacity-100' : 'max-h-0 opacity-0'} overflow-hidden`}>
                 <nav className="flex flex-col items-center justify-center bg-neutral-200 dark:bg-neutral-700 text-neutral-900 dark:text-neutral-50 space-y-2 py-2">
-                    <Link  to="/" onClick={handleOnClick} className="px-4 py-2 hover:bg-neutral-300 hover:dark:bg-neutral-600 rounded font-medium">Home</Link >
-                    <Link  to="/search" onClick={handleOnClick} className="px-4 py-2 hover:bg-neutral-300 hover:dark:bg-neutral-600 rounded font-medium">Buscar</Link >
-                    <Link  to="/review" onClick={handleOnClick} className="px-4 py-2 hover:bg-neutral-300 hover:dark:bg-neutral-600 rounded font-medium">Reseña</Link >
-                    <Link  to="/profile" onClick={handleOnClick} className="px-4 py-2 hover:bg-neutral-300 hover:dark:bg-neutral-600 rounded font-medium">Perfil</Link >
+                    <Link to="/" onClick={handleOnClick} className="px-4 py-2 hover:bg-neutral-300 hover:dark:bg-neutral-600 rounded font-medium">Home</Link >
+                    <Link to="/search" onClick={handleOnClick} className="px-4 py-2 hover:bg-neutral-300 hover:dark:bg-neutral-600 rounded font-medium">Buscar</Link >
+                    {isAdmin
+                        ? <Link to="/admin" onClick={handleOnClick} className="px-4 py-2 hover:bg-neutral-300 hover:dark:bg-neutral-600 rounded font-medium">Gestión</Link >
+                        : <>
+                             <Link to="/review" onClick={handleOnClick} className="px-4 py-2 hover:bg-neutral-300 hover:dark:bg-neutral-600 rounded font-medium">Reseña</Link >
+                            <Link to="/profile" onClick={handleOnClick} className="px-4 py-2 hover:bg-neutral-300 hover:dark:bg-neutral-600 rounded font-medium">Perfil</Link >
+                          </>
+                    }
                 </nav>
             </div>
         </header>
